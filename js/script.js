e2785a4b3eefb8fa657fe4a30ccab1fab889dd32
CodesofVysakh <ovysakh309@gmail.com>
2021-09-11 09:42:20 +0530

    added owl-carousel

--- js/script.js
+++ js/script.js
@@ -57,6 +57,16 @@ mode.addEventListener("click", ()=>{
         element.classList.toggle("active");
     })
     
-    
 })
 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        nav: true,
+    });
+
+  });
